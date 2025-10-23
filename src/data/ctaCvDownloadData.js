b1b780eb5d096a0f4e14d83ec01b6a4a781537b9
Simón Ibalo <simonibalo@gmail.com>
2025-10-23 08:48:28 -0300

    minor dot to try redeploy

--- src/data/ctaCvDownloadData.js
+++ src/data/ctaCvDownloadData.js
@@ -1,13 +1,13 @@
 export const CtaCvDownloadContent = {
     EN: {
       title: "My Resume",
-      description: "Click here and download my resume",
+      description: "Click here and download my resume.",
       btnText: "Download",
       btnUrl: `${process.env.PUBLIC_URL}/assets/downloads/simon-ibalo-ui-engineer-en.pdf`
     },
     ES: {
       title: "Mi Currículum",
-      description: "Haz clic aquí y descarga mi currículum",
+      description: "Haz clic aquí y descarga mi currículum.",
       btnText: "Descargar",
       btnUrl: `${process.env.PUBLIC_URL}/assets/downloads/simon-ibalo-ui-engineer-es.pdf`
     }
